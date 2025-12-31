924a63af41f6372ea9f6fe83bb328d88d1a500e3
neerajann <neerajan.vfx@gmail.com>
2025-12-31 22:37:44 +0545

    feat( dns-server) : add support for handling MX-type DNS queries

--- dns-server/server.js
+++ dns-server/server.js
@@ -3,7 +3,7 @@ import dnsPacket from 'dns-packet'
 import { MongoClient } from 'mongodb'
 import env from 'dotenv'
 
-env.config()
+env.config({ quiet: true })
 
 const server = dgram.createSocket('udp4')
 const upstream = dgram.createSocket('udp4')
@@ -16,6 +16,7 @@ const records = db.collection('records')
 const upstreamdns = db.collection('upstreamdns')
 const blockedCollection = db.collection('blocklist')
 
+const pendingRequests = new Map()
 const blockedDomains = new Set()
 
 await loadBlockList()
@@ -37,17 +38,17 @@ server.on('message', async (msg, rinfo) => {
       return sendResponse({
         incomingMessage,
         rinfo,
-        ptr: true,
       })
     }
     const recordFromDB = await findRecord(incomingMessage)
+
     if (!recordFromDB) {
       return await forwardToUpStream(msg, rinfo)
     } else {
       return sendResponse({
         incomingMessage,
         rinfo,
-        recordFromDB: recordFromDB,
+        recordFromDB,
       })
     }
   } catch (error) {
@@ -82,49 +83,86 @@ const findRecord = async (incomingMessage) => {
     }
   )
 }
-const sendResponse = ({
-  incomingMessage,
-  rinfo,
-  recordFromDB,
-  ptr,
-  blocked,
-}) => {
-  let data = recordFromDB?.records[0]?.content
-  let type = incomingMessage.questions[0].type
-  if (ptr) {
-    type = 'PTR'
-    data = process.env.DNS_DOMAIN
-  }
+const sendResponse = ({ incomingMessage, rinfo, recordFromDB, blocked }) => {
+  let answers
+  const questions = incomingMessage.questions[0]
+  const qtype = questions.type
   if (blocked) {
-    data = '0.0.0.0'
-    type = incomingMessage.questions[0].type
+    answers = [
+      {
+        data: '0.0.0.0',
+        type: 'A',
+        name: questions.name,
+        ttl: 10,
+        class: 'IN',
+      },
+    ]
+  } else if (qtype == 'PTR') {
+    answers = [
+      {
+        type: qtype,
+        data: process.env.DNS_DOMAIN,
+        name: questions.name,
+        ttl: 10,
+        class: 'IN',
+      },
+    ]
+  } else if (qtype == 'MX') {
+    answers = [
+      {
+        type: qtype,
+        class: 'IN',
+        name: questions.name,
+        ttl: 10,
+        data: {
+          preference: recordFromDB.records[0].priority ?? 10,
+          exchange: recordFromDB.records[0].content,
+        },
+      },
+    ]
+  } else {
+    answers = [
+      {
+        type: qtype,
+        class: 'IN',
+        name: questions.name,
+        ttl: 50,
+        data: recordFromDB.records[0].content,
+      },
+    ]
   }
 
   const answer = dnsPacket.encode({
     id: incomingMessage.id,
     type: 'response',
     flags: dnsPacket.AUTHORITATIVE_ANSWER,
-    questions: incomingMessage.questions,
-    answers: [
-      {
-        type,
-        class: 'IN',
-        name: incomingMessage.questions[0].name,
-        ttl: 300,
-        data,
-      },
-    ],
+    questions: [questions],
+    answers,
   })
   server.send(answer, rinfo.port, rinfo.address)
 }
 
 const forwardToUpStream = async (msg, rinfo) => {
   const UPSTREAM_DNS = await upstreamdns.find({}).toArray()
-  upstream.send(msg, UPSTREAM_DNS[1].port, UPSTREAM_DNS[1].address)
-  upstream.on('message', (response) => {
-    server.send(response, rinfo.port, rinfo.address)
+
+  const decoded = dnsPacket.decode(msg)
+  pendingRequests.set(decoded.id, {
+    address: rinfo.address,
+    port: rinfo.port,
   })
+
+  upstream.send(msg, UPSTREAM_DNS[1].port, UPSTREAM_DNS[1].address)
 }
+
+upstream.on('message', (response) => {
+  const decoded = dnsPacket.decode(response)
+  const request = pendingRequests.get(decoded.id)
+
+  if (!request) return
+
+  server.send(response, request.port, request.address)
+  pendingRequests.delete(decoded.id)
+})
 
 server.bind(53, () => {
   console.log('DNS Server running ')
